72e49b22461c0f7b4c045181ef783d2f04d67ae5
lfqaEWFC <https://github.com/lfqaEWFC/mytest1.git>
2024-11-09 15:40:22 +0800

    编程题

---
+++ 第十二章 编程练习题5.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+#include<stdlib.h>
+void my_rand(void){
+    int a = 0,i,j,mid;
+    int arr[100];
+    for(i=0;i<100;i++){
+        a = rand()%10+1;
+        arr[i]=a;
+    }
+    for(i=0;i<100;i++){
+        for(j = 0;j<100-i-1;j++){
+            if(arr[j]<arr[j+1]){
+                mid = arr[j];
+                arr[j]=arr[j+1];
+                arr[j+1]=mid; 
+            }
+        }
+    }
+    for(i=0;i<100;i++){
+        printf("%d\t",arr[i]);
+    }
+}
+
+
+
+int main(){
+
+    my_rand();
+    
+}
